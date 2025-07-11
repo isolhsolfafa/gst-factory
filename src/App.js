70eb0cd728b1d68b12a55ebc8fbbf557ef1819bd
isolhsolfafa <angdredong@gmail.com>
2025-07-11 13:30:49 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation, useNavigate } from 'react-router-dom'; 
 import axios from 'axios';
 import WeeklyChart from './components/WeeklyChart';
 import MonthlyChart from './components/MonthlyChart';
